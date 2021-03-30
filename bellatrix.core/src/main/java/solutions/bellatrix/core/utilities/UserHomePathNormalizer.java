704730765b78f20da8ab3271ead474ef7c2fbdaa
Teodor Nikolov <teo8447@gmail.com>
2021-03-30 14:48:42 +0300

    fixed UserHomePathNormalizer cross-platform checks

--- bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/UserHomePathNormalizer.java
+++ bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/UserHomePathNormalizer.java
@@ -4,24 +4,20 @@ import java.nio.file.Paths;
 
 public class UserHomePathNormalizer {
     public static String normalizePath(String path) {
-        if (path.startsWith("user.home")) {
-            path = Paths.get(getUserHomePath(), path.substring(9)).toString();
-        }
-
-        if (RuntimeInformation.IS_MAC && RuntimeInformation.IS_UNIX) {
+        if (RuntimeInformation.IS_MAC || RuntimeInformation.IS_UNIX) {
             path = path.replace('\\', '/');
         } else if (RuntimeInformation.IS_WINDOWS) {
             path = path.replace('/', '\\');
+        }
+
+        if (path.startsWith("user.home")) {
+            path = Paths.get(getUserHomePath(), path.substring(9)).toString();
         }
 
         return path;
     }
 
     public static String getUserHomePath() {
-        String userHomePath = System.getProperty("user.home");
-        if (userHomePath.endsWith("/") || userHomePath.endsWith("\\")) {
-            userHomePath = userHomePath.substring(0, userHomePath.length() - 1);
-        }
-        return userHomePath;
+        return System.getProperty("user.home");
     }
 }
