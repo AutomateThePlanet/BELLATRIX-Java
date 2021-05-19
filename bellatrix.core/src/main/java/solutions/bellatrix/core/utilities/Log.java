30702b6c95d43dc89e040648f6aa703a5bee7476
Teodor Nikolov <teo8447@gmail.com>
2021-05-19 12:51:17 +0300

    added event listener to logger

--- bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/Log.java
+++ bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/Log.java
@@ -1,14 +1,20 @@
 package solutions.bellatrix.core.utilities;
 
 import org.jetbrains.annotations.NotNull;
+import solutions.bellatrix.core.plugins.EventListener;
 
 public class Log {
     static private final Object lock = new Object();
+    public final static EventListener<LoggerActionEventArgs> LOGGED_ENTRY = new EventListener<>();
+    public final static EventListener<LoggerActionEventArgs> LOGGED_INFO = new EventListener<>();
+    public final static EventListener<LoggerActionEventArgs> LOGGED_ERROR = new EventListener<>();
 
     public static void info(@NotNull String format, Object... args) {
         Thread waitThread = new Thread(Log::staticWait);
         Thread printThread = new Thread(() -> {
             String info = String.format(format, args);
+            LOGGED_ENTRY.broadcast(new LoggerActionEventArgs(info));
+            LOGGED_INFO.broadcast(new LoggerActionEventArgs(info));
             System.out.println(info);
         });
         printThread.start();
@@ -19,6 +25,8 @@ public class Log {
         Thread waitThread = new Thread(Log::staticWait);
         Thread printThread = new Thread(() -> {
             String error = String.format(format, args);
+            LOGGED_ENTRY.broadcast(new LoggerActionEventArgs(error));
+            LOGGED_ERROR.broadcast(new LoggerActionEventArgs(error));
             System.err.println(error);
             staticNotify();
         });
