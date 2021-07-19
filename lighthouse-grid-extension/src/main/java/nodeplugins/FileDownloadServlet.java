0e88d13f8e0a9fd07e68c35dae28d3f281ac3d39
Anton Angelov <aangelov@automatetheplanet.com>
2021-07-19 14:55:57 +0300

    add new selenium grid extensions project

---
+++ lighthouse-grid-extension/src/main/java/nodeplugins/FileDownloadServlet.java
@@ -0,0 +1,73 @@
+package nodeplugins;
+
+import com.google.common.net.HttpHeaders;
+import com.google.common.net.MediaType;
+import org.apache.commons.io.IOUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Base64;
+import java.util.logging.Logger;
+
+public class FileDownloadServlet extends HttpServlet {
+
+    private static final Logger LOGGER = Logger.getLogger(FileDownloadServlet.class.getName());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String pathInfo = req.getRequestURI().substring(req.getServletPath().length() + 1);
+        pathInfo = new String(Base64.getUrlDecoder().decode(pathInfo.getBytes()));
+        LOGGER.info("Request for file download received with path: " + pathInfo);
+
+        File file = new File(pathInfo);
+        if (!fileExistsAndNotDirectory(file, resp)) {
+            return;
+        }
+
+        String contentType = identifyFileContentType(file);
+        resp.setContentType(contentType);
+        resp.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+
+        try (
+                FileInputStream fileInputStream = new FileInputStream(file);
+                ServletOutputStream outputStream = resp.getOutputStream()) {
+            IOUtils.copy(fileInputStream, outputStream);
+        }
+    }
+
+    private String identifyFileContentType(File file) throws IOException {
+        String contentType = Files.probeContentType(file.toPath());
+        return contentType != null ? contentType : MediaType.OCTET_STREAM.toString();
+    }
+
+    private boolean fileExistsAndNotDirectory(File requestedFile, HttpServletResponse resp) throws IOException {
+        if (!requestedFile.exists()) {
+            LOGGER.info("Requested file doesn't exist: " + requestedFile.getAbsolutePath());
+            sendError(resp, "Requested file doesn't exist.");
+            return false;
+        }
+        if (requestedFile.isDirectory()) {
+            LOGGER.info("Requested file is directory: " + requestedFile.getAbsolutePath());
+            sendError(resp, "Requested file is directory.");
+            return false;
+        }
+        if (!requestedFile.canRead()) {
+            LOGGER.info("Requested file cannot bet read: " + requestedFile.getAbsolutePath());
+            sendError(resp, "Requested file can be read.");
+            return false;
+        }
+        return true;
+    }
+
+    private void sendError(HttpServletResponse resp, String msg) throws IOException {
+        resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        resp.getWriter().write(msg);
+    }
+}
