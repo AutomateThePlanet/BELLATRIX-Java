b6e4129fb8631ae34fbb0f3cb10a89c491849034
Nikolay Avramov <navramov@plex.com>
2023-03-30 09:05:02 +0300

    fix issues with configs and scroll to visible

--- bellatrix.web/src/main/java/solutions/bellatrix/web/components/WebComponent.java
+++ bellatrix.web/src/main/java/solutions/bellatrix/web/components/WebComponent.java
@@ -1033,7 +1033,7 @@ public class WebComponent extends LayoutComponentValidationsBuilder implements C
     private void scrollToVisible(WebElement wrappedElement, boolean shouldWait) {
         SCROLLING_TO_VISIBLE.broadcast(new ComponentActionEventArgs(this));
         try {
-            javaScriptService.execute("arguments[0].scrollIntoView(true);", wrappedElement);
+            javaScriptService.execute("arguments[0].scrollIntoView({ block: \"center\" });", wrappedElement);
             if (shouldWait) {
                 Thread.sleep(500);
                 toExist().waitToBe();
