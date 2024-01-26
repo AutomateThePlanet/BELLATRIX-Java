6ef22d4b30dcd27a614653eafbd99485f3a3b631
NikolayAvramov <navramov@automatetheplanet.com>
2024-01-26 10:43:33 +0200

    add try catch in wait for page load

--- bellatrix.web/src/main/java/solutions/bellatrix/web/services/BrowserService.java
+++ bellatrix.web/src/main/java/solutions/bellatrix/web/services/BrowserService.java
@@ -248,7 +248,12 @@ public class BrowserService extends WebService {
         long waitUntilReadyTimeout = ConfigurationService.get(WebSettings.class).getTimeoutSettings().getWaitUntilReadyTimeout();
         long sleepInterval = ConfigurationService.get(WebSettings.class).getTimeoutSettings().getSleepInterval();
         var webDriverWait = new WebDriverWait(getWrappedDriver(), Duration.ofSeconds(waitUntilReadyTimeout), Duration.ofSeconds(sleepInterval));
-        webDriverWait.until(webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+        try {
+            webDriverWait.until(webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+        }
+        catch (ScriptTimeoutException ex) {
+            Log.error("Script timeout while loading for page load.");
+        }
     }
 
     public void injectInfoNotificationToast(String message) {
