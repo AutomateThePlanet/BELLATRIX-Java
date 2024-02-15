a901b3049265340ca7db948cbd78560d345059d3
Teodor Nikolov <teo8447@gmail.com>
2024-02-15 09:24:38 +0200

    updated DriverService to use SecretsResolver for environment variables

--- bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/SecretsResolver.java
+++ bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/SecretsResolver.java
@@ -12,31 +12,32 @@
  */
 package solutions.bellatrix.core.utilities;
 
+import java.util.HashMap;
 import java.util.function.Supplier;
 
 public class SecretsResolver {
+    private static final ThreadLocal<HashMap<String, String>> CACHED_SECRETS;
+
+    static {
+        CACHED_SECRETS = ThreadLocal.withInitial(HashMap::new);
+        CACHED_SECRETS.set(new HashMap<>());
+    }
+
     public static String getSecret(Supplier<String> getConfigValue) {
-        if (getConfigValue.get().contains("env_"))
-        {
-            String envName = getConfigValue.get().replace("{env_", "").replace("}", "");
-            String environmentalVariable = System.getenv(envName);
-            return environmentalVariable;
-        }
-        else
-        {
-            return getConfigValue.get();
-        }
+        return getSecret(getConfigValue.get());
     }
 
     public static String getSecret(String configValue) {
-        if (configValue.contains("env_"))
-        {
-            String envName = configValue.replace("{env_", "").replace("}", "");
-            String environmentalVariable = System.getenv(envName);
-            return environmentalVariable;
+        if (CACHED_SECRETS.get().containsKey(configValue)) {
+            return CACHED_SECRETS.get().get(configValue);
         }
-        else
-        {
+
+        if (configValue.contains("env_")) {
+            var envName = configValue.replace("{env_", "").replace("}", "").toLowerCase();
+            String environmentalVariable = System.getenv(envName);
+            CACHED_SECRETS.get().put(configValue, environmentalVariable);
+            return environmentalVariable;
+        } else {
             return configValue;
         }
     }
