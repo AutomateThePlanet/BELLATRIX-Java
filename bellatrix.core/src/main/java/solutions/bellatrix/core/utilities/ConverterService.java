68642a00436b7cd933ce75a3805dbe21569a3384
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2024-03-20 01:06:20 +0200

    Renamed GenericUtilities class in core to ConverterService

--- bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/ConverterService.java
+++ bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/ConverterService.java
@@ -2,8 +2,7 @@ package solutions.bellatrix.core.utilities;
 
 import lombok.SneakyThrows;
 
-// ToDo This class needs to be removed and the methods inside moved somewhere else
-public class GenericUtilities {
+public class ConverterService {
     // ToDo Dozer must be used instead
     @SneakyThrows
     public static <SourceT, ResultT> ResultT convert(SourceT source, int index) {
@@ -16,6 +15,28 @@ public class GenericUtilities {
     @SneakyThrows
     public static <SourceT, ResultT> ResultT convert(SourceT source, ResultT result) {
         var object = (ResultT) InstanceFactory.create(result.getClass());
+
+        var sourceFields = source.getClass().getDeclaredFields();
+        var objectFields = object.getClass().getDeclaredFields();
+
+        for (var sourceField : sourceFields) {
+            for (var objectField : objectFields) {
+                if (sourceField.getName().equals(objectField.getName())) {
+                    sourceField.setAccessible(true);
+                    objectField.setAccessible(true);
+
+                    objectField.set(object, sourceField.get(source));
+                    break;
+                }
+            }
+        }
+
+        return object;
+    }
+
+    @SneakyThrows
+    public static <SourceT, ResultT> ResultT convertToClass(SourceT source, Class<ResultT> result) {
+        var object = (ResultT) InstanceFactory.create(result);
 
         var sourceFields = source.getClass().getDeclaredFields();
         var objectFields = object.getClass().getDeclaredFields();
