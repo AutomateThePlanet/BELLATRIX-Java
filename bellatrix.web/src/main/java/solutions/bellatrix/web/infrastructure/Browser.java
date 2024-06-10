9895f7e65d9b5c082efd4d417d376f8650073142
NikolayAvramov <navramov@automatetheplanet.com>
2024-06-10 09:25:31 +0300

    add chrome responsive to browser types

--- bellatrix.web/src/main/java/solutions/bellatrix/web/infrastructure/Browser.java
+++ bellatrix.web/src/main/java/solutions/bellatrix/web/infrastructure/Browser.java
@@ -19,6 +19,7 @@ public enum Browser {
     CHROME("chrome"),
     CHROME_HEADLESS("chrome_headless"),
     CHROME_MOBILE("chrome_mobile"),
+    CHROME_RESPONSIVE("chrome_responsive"),
     FIREFOX("firefox"),
     FIREFOX_HEADLESS("firefox_headless"),
     EDGE("edge"),
