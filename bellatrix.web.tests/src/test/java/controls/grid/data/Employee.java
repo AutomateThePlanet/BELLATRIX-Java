ec3d60d74456742d590878937d686e501331ab39
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2024-06-14 17:42:02 +0300

    Added tests for Grid component

---
+++ bellatrix.web.tests/src/test/java/controls/grid/data/Employee.java
@@ -0,0 +1,21 @@
+package controls.grid.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import solutions.bellatrix.web.components.advanced.TableHeader;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class Employee {
+    @TableHeader(name = "Order")
+    public String order;
+    @TableHeader(name = "Firstname")
+    public String firstName;
+    @TableHeader(name = "Lastname")
+    public String lastName;
+    @TableHeader(name = "Email Business")
+    public String businessEmail;
+    @TableHeader(name = "Email Personal")
+    public String personalEmail;
+}
