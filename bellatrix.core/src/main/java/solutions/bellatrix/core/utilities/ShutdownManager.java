94174a14771f4c05854fa8905e5c74ee0825efa6
MiriamKyoseva <miriam.kyoseva1@gmail.com>
2024-07-15 18:11:02 +0300

    Update ShutdownManager.java

--- bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/ShutdownManager.java
+++ bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/ShutdownManager.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2024 Automate The Planet Ltd.
+ * Author: Miriam Kyoseva
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package solutions.bellatrix.core.utilities;
 
 import lombok.experimental.UtilityClass;
@@ -7,7 +20,6 @@ import java.util.List;
 
 @UtilityClass
 public class ShutdownManager {
-    private static final List<AutoCloseable> resources = new ArrayList<>();
     private static final List<Runnable> instructions = new ArrayList<>();
 
     static {
