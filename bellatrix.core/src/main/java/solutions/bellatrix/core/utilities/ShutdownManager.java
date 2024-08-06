c57e4b385046f3fd9eedc773d5c9ed066efa5466
MiriamKyoseva <133047546+MiriamKyoseva@users.noreply.github.com>
2024-08-06 18:59:41 +0300

    Update ShutdownManager.java

--- bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/ShutdownManager.java
+++ bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/ShutdownManager.java
@@ -17,10 +17,15 @@ import lombok.experimental.UtilityClass;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @UtilityClass
 public class ShutdownManager {
-    private static final List<Runnable> instructions = new ArrayList<>();
+    private static final List<Runnable> instructions = new CopyOnWriteArrayList<>();
+    private static final ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     static {
         Runtime.getRuntime().addShutdownHook(new Thread(ShutdownManager::runAllInstructions));
@@ -33,11 +38,38 @@ public class ShutdownManager {
     private static void runAllInstructions() {
         if (instructions.isEmpty()) return;
 
-        for (var instruction : instructions) {
-            try {
-                instruction.run();
-            } catch (Exception ignored) {
+        try {
+            // Submit all instructions for parallel execution
+            for (var instruction : instructions) {
+                executor.submit(() -> {
+                    try {
+                        instruction.run();
+                    } catch (Exception ex) {
+                        DebugInformation.debugInfo(ex.getMessage());
+                    }
+                });
             }
+        } finally {
+            shutdownAndAwaitTermination(executor);
+        }
+    }
+
+    private static void shutdownAndAwaitTermination(ExecutorService executor) {
+        executor.shutdown(); // Disable new tasks from being submitted
+        try {
+            // Wait a while for existing tasks to terminate
+            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                executor.shutdownNow(); // Cancel currently executing tasks
+                // Wait a while for tasks to respond to being cancelled
+                if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
+                    System.err.println("Executor did not terminate.");
+                }
+            }
+        } catch (InterruptedException ie) {
+            // (Re-)Cancel if current thread also interrupted
+            executor.shutdownNow();
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
         }
     }
 }
