ba4d7d3fec777ece7a6ef3f55d004be310d63279
Teodor Nikolov <teo8447@gmail.com>
2024-11-11 12:40:39 +0200

    updated getting started

--- getting-started/bellatrix.web.getting.started/src/test/java/O25_openCv_image_actions/data/enums/EncodedImageDemo.java
+++ getting-started/bellatrix.web.getting.started/src/test/java/O25_openCv_image_actions/data/enums/EncodedImageDemo.java
@@ -4,8 +4,17 @@ import O25_openCv_image_actions.data.ImageInBase64;
 import solutions.bellatrix.web.infrastructure.Base64Encodable;
 
 public enum EncodedImageDemo implements Base64Encodable {
+    /**
+     * <img src={@value ImageInBase64#falcon9}/>
+     */
     FALCON_9("falcon9", ImageInBase64.falcon9, 55, 70),
+    /**
+     * <img src={@value ImageInBase64#falcon9BackButton}/>
+     */
     FALCON_9_BACK_BUTTON("falcon9BackButton", ImageInBase64.falcon9BackButton, 20, 5),
+    /**
+     * <img src={@value ImageInBase64#commentTextArea}/>
+     */
     COMMENT_TEXT_AREA("commentTextArea", ImageInBase64.commentTextArea, 30, 50);
 
     private final String imageName;
