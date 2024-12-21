f1ca853e5cb8fa168cc64eb97e68ce60ef35dc13
MiriamKyoseva <miriam.kyoseva1@gmail.com>
2024-12-21 15:32:04 +0200

    Update ObjectFactory.java

--- bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/ObjectFactory.java
+++ bellatrix.core/src/main/java/solutions/bellatrix/core/utilities/ObjectFactory.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2024 Automate The Planet Ltd.
+ * Author: Miriyam Kyoseva
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package solutions.bellatrix.core.utilities;
 
 import java.lang.reflect.Constructor;
@@ -31,11 +45,10 @@ public abstract class ObjectFactory {
             }
 
             throw new ConstructorNotFoundException(("""
-                No constructor with the specified parameters was found.
-                Given argument count: %d
-                Given argument types: %s
-                %s
-                """).formatted(argumentTypes.length, types.toString(), e)
+                    No constructor with the specified parameters was found.
+                    Given argument count: %d
+                    Given argument types: %s
+                    """).formatted(argumentTypes.length, types.toString())
             );
         }
     }
@@ -77,7 +90,7 @@ public abstract class ObjectFactory {
                 return true;
             }
 
-            if (!argumentTypes[i].equals(parameters[i].getType())) {
+            if (argumentTypes.length - 1 < i || !argumentTypes[i].equals(parameters[i].getType())) {
                 return false;
             }
         }
