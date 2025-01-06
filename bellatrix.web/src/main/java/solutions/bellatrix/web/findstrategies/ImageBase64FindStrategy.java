c2418fff2c7ba72c3f2ed61f3c6626c70ebd64da
MiriamKyoseva <miriam.kyoseva1@gmail.com>
2025-01-06 12:57:35 +0200

    Moved ByImageBase64 in ImageBase64FindStrategy as a static nested class
    It is not supposed to be used outside of the context of ImageBase64FindStrategy;
    furthermore, it has no place in 'infrastructure' folder

--- bellatrix.web/src/main/java/solutions/bellatrix/web/findstrategies/ImageBase64FindStrategy.java
+++ bellatrix.web/src/main/java/solutions/bellatrix/web/findstrategies/ImageBase64FindStrategy.java
@@ -2,8 +2,11 @@ package solutions.bellatrix.web.findstrategies;
 
 import lombok.Getter;
 import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
 import solutions.bellatrix.plugins.opencv.Base64Encodable;
-import solutions.bellatrix.web.infrastructure.ByImageBase64;
+
+import java.util.List;
 
 @Getter
 public class ImageBase64FindStrategy extends FindStrategy {
@@ -22,5 +25,26 @@ public class ImageBase64FindStrategy extends FindStrategy {
     @Override
     public String toString() {
         return String.format("image base 64: %s", encodedImage.getBase64Image());
+    }
+
+    public static class ByImageBase64 extends By {
+        private final String base64EncodedImage;
+
+        public ByImageBase64(String base64EncodedImage) {
+            this.base64EncodedImage = base64EncodedImage;
+        }
+
+        public static By byImageBase64(String encodedImage) {
+            return new ByImageBase64(encodedImage);
+        }
+
+        @Override
+        public List<WebElement> findElements(SearchContext context) {
+            return null;
+        }
+
+        public String toString() {
+            return "By.imageBase64: " + this.base64EncodedImage;
+        }
     }
 }
