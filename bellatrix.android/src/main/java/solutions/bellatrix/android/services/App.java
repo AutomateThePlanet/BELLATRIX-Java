0aa617695f08f18fa14e68002aedb11dfa43a6c6
ivaylo.dimitrov <ivaylo.dimitrov@nuvolo.com>
2025-03-17 14:24:17 +0200

    Removed unused bracket

--- bellatrix.android/src/main/java/solutions/bellatrix/android/services/App.java
+++ bellatrix.android/src/main/java/solutions/bellatrix/android/services/App.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2022 Automate The Planet Ltd.
  * Author: Anton Angelov
- * Licensed under the Apache License, Version 2.0 (the "License"));
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
